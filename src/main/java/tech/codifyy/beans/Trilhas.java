d5581c69eae3d2c4fe9ca2b4e5bab7c6e84e4e07
VitorMantovani <mantovanifreitasvitor@gmail.com>
2022-10-08 11:54:49 -0300

    Classe Trilhas atualizada

--- src/main/java/tech/codifyy/beans/Trilhas.java
+++ src/main/java/tech/codifyy/beans/Trilhas.java
@@ -7,4 +7,83 @@ public class Trilhas {
     private String title, author, description, category, url;
     private List<String> topics;
 
+    private Tecnologia technology;
+
+    public Trilhas () {
+    }
+
+    public Trilhas(Integer _id, String title, String author, String description, String category, String url, List<String> topics, Tecnologia technology) {
+        this._id = _id;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.category = category;
+        this.url = url;
+        this.topics = topics;
+        this.technology = technology;
+    }
+
+    public Integer get_id() {
+        return _id;
+    }
+
+    public void set_id(Integer _id) {
+        this._id = _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<String> topics) {
+        this.topics = topics;
+    }
+
+    public Tecnologia getTechnology() {
+        return technology;
+    }
+
+    public void setTechnology(Tecnologia technology) {
+        this.technology = technology;
+    }
 }
